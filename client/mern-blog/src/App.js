1f4633b79357bcdcbf81327cc78addae3b2b469b
Maryam Navaei <maryam.navaei@gmail.com>
2023-12-31 12:18:31 -0500

    adding Layout

--- client/mern-blog/src/App.js
+++ client/mern-blog/src/App.js
@@ -3,19 +3,15 @@ import Header from './components/Header';
 import Post from './components/Post';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <Routes>
-      <Route index element={<main>
-        <Header />
-        <Post />
-        <Post />
-        <Post />
-      </main>} />
-      <Route path={'/login'} element={
-        <div>Login page</div>
-      } />
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Post />} />
+        <Route path={'/login'} element={<div>Login page</div>} />
+      </Route>
     </Routes>
   );
 }
