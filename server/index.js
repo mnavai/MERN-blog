8bcf1eaee834ff95ec6a11fb21cdbaef7305a048
Maryam Navaei <maryam.navaei@gmail.com>
2023-12-31 16:44:58 -0500

    connected mongoDB

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect('mongodb+srv://blogger:8XoYG0n44rxta3fX@cluster0.2tiperl.mongodb.net/?retryWrites=true&w=majority');
 
 app.post('/register',(req,res) =>{
     const {username, password} = req.body;
@@ -11,3 +14,5 @@ app.post('/register',(req,res) =>{
 });
 
 app.listen(4000);
+// blogger 8XoYG0n44rxta3fX
+//mongodb+srv://blogger:8XoYG0n44rxta3fX@cluster0.2tiperl.mongodb.net/?retryWrites=true&w=majority
