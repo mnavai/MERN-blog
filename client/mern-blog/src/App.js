140214353954dc81bcec001c2460ce5eba40fb73
Maryam Navaei <maryam.navaei@gmail.com>
2023-12-31 12:08:02 -0500

    adding routers

--- client/mern-blog/src/App.js
+++ client/mern-blog/src/App.js
@@ -1,55 +1,22 @@
+import React from 'react';
+import Header from './components/Header';
+import Post from './components/Post';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">MyBlog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2021/05/GettyImages-1209495734.jpg?w=430&h=230&crop=1" alt="ai" />
-        </div>
-        <div className="texts">
-          <h2>Giga ML wants to help companies deploy LLMs offline</h2>
-          <p className="info">
-            <a className="author">Maryam Navaei</a>
-            <time>2024-12-31 11:45</time>
-          </p>
-          <p className="summary">AI is all the rage — particularly text-generating AI, also known as large language models (think models along the lines of ChatGPT). In one recent survey of ~1,000 enterprise organizations, 67.2% say that they see adopting large language models (LLMs) as a top priority by early 2024.</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2021/05/GettyImages-1209495734.jpg?w=430&h=230&crop=1" alt="ai" />
-        </div>
-        <div className="texts">
-          <h2>Giga ML wants to help companies deploy LLMs offline</h2>
-          <p className="info">
-            <a className="author">Maryam Navaei</a>
-            <time>2024-12-31 11:45</time>
-          </p>
-          <p className="summary">AI is all the rage — particularly text-generating AI, also known as large language models (think models along the lines of ChatGPT). In one recent survey of ~1,000 enterprise organizations, 67.2% say that they see adopting large language models (LLMs) as a top priority by early 2024.</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2021/05/GettyImages-1209495734.jpg?w=430&h=230&crop=1" alt="ai" />
-        </div>
-        <div className="texts">
-          <h2>Giga ML wants to help companies deploy LLMs offline</h2>
-          <p className="info">
-            <a className="author">Maryam Navaei</a>
-            <time>2024-12-31 11:45</time>
-          </p>
-          <p className="summary">AI is all the rage — particularly text-generating AI, also known as large language models (think models along the lines of ChatGPT). In one recent survey of ~1,000 enterprise organizations, 67.2% say that they see adopting large language models (LLMs) as a top priority by early 2024.</p>
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route index element={<main>
+        <Header />
+        <Post />
+        <Post />
+        <Post />
+      </main>} />
+      <Route path={'/login'} element={
+        <div>Login page</div>
+      } />
+    </Routes>
   );
 }
 
---
+++ client/mern-blog/src/components/Post.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <div className="post">
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2021/05/GettyImages-1209495734.jpg?w=430&h=230&crop=1" alt="ai" />
+        </div>
+        <div className="texts">
+          <h2>Giga ML wants to help companies deploy LLMs offline</h2>
+          <p className="info">
+            <a className="author">Maryam Navaei</a>
+            <time>2024-12-31 11:45</time>
+          </p>
+          <p className="summary">AI is all the rage — particularly text-generating AI, also known as large language models (think models along the lines of ChatGPT). In one recent survey of ~1,000 enterprise organizations, 67.2% say that they see adopting large language models (LLMs) as a top priority by early 2024.</p>
+        </div>
+      </div>
+  )
+}
+
+export default Post
