619b14d38c6ed945b696e62f0d0f14c876df9714
Maryam Navaei <maryam.navaei@gmail.com>
2023-12-31 13:27:58 -0500

    added Login and Register pages

--- client/mern-blog/src/App.js
+++ client/mern-blog/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import Header from './components/Header';
-import Post from './components/Post';
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
 import Layout from './components/Layout';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import './App.css';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
-        <Route index element={<Post />} />
-        <Route path={'/login'} element={<div>Login page</div>} />
+        <Route index element={<Home />} />
+        <Route path={'/login'} element={<Login />} />
+        <Route path={'/register'} element={<Register />} />
       </Route>
     </Routes>
   );
