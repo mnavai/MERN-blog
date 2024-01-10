73f3f9946de310651d5f09b03950cbed9d9b4107
Maryam Navaei <maryam.navaei@gmail.com>
2024-01-09 23:04:35 -0500

    login page updated

--- client/mern-blog/src/pages/Login.js
+++ client/mern-blog/src/pages/Login.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <form className='login'>
         <h1>Login</h1>
-        <input type='text' placeholder='Username' />
-        <input type='password' placeholder='Password'/>
+        <input type='text' placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
         <button>Login</button>
     </form>
   )
