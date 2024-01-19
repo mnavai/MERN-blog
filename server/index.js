f7ddcc353f6fb205c7e8ef1812a41031ce704c85
Maryam Navaei <maryam.navaei@gmail.com>
2024-01-18 23:25:02 -0500

    latest changes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const { default: mongoose } = require('mongoose');
 const User = require('./models/User')
 const bcrypt = require('bcryptjs')
 const app = express();
+const jwt = require('jsonwebtoken');
+const { default: mongoose } = require('mongoose');
 
 const salt = bcrypt.genSaltSync(10)
+const secret = 'dsjfgfhdgdgdfgkdfbndvjbd'
 
 app.use(cors());
 app.use(express.json());
@@ -21,6 +23,7 @@ app.post('/register',async (req,res) =>{
         })
         res.json(userDoc)
     } catch(e){
+        console.log(e)
         res.status(400).json(e)
     }
 });
@@ -30,6 +33,15 @@ app.post('/login', async (req, res) => {
     const userDoc = await User.findOne({username:username})
     const passOk = bcrypt.compareSync(password, userDoc.password)
     res.json(passOk)
+    if (passOk){
+        jwt.sign({username, id:userDoc._id},secret, {}, (err,token) => {
+            if (err) throw err;
+            res.json(token)
+        })
+        res.json()
+    } else {
+        res.status(400).json('wrong credential')
+    }
 })
 
 app.listen(4000);
