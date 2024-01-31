3af8619027c2f775bc0deed1303f7d12edce52e6
Maryam Navaei <maryam.navaei@gmail.com>
2024-01-31 11:05:29 -0500

    added cookie

--- client/mern-blog/src/pages/Login.js
+++ client/mern-blog/src/pages/Login.js
@@ -9,7 +9,8 @@ const Login = () => {
     await fetch('http://localhost:4000/login', {
       method: 'POST',
       body: JSON.stringify({ username, password }),
-      headers: { 'Content-Type': 'application/json' }, 
+      headers: { 'Content-Type': 'application/json' },
+      
     });
   };
 
