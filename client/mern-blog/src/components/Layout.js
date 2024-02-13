885a1a0118c8db5123d060178aad7c7fc316fd92
Maryam Navaei <maryam.navaei@gmail.com>
2024-02-12 23:46:43 -0500

    Layout updated

--- client/mern-blog/src/components/Layout.js
+++ client/mern-blog/src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const Layout = () => {
   return (
