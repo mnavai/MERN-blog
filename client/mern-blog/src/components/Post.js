52ff085544c549778d240f27b194e8f4a19ed57a
Maryam Navaei <maryam.navaei@gmail.com>
2024-02-16 22:13:58 -0500

    latest changes

--- client/mern-blog/src/components/Post.js
+++ client/mern-blog/src/components/Post.js
@@ -12,7 +12,7 @@ const Post = () => {
             <a className="author">Maryam Navaei</a>
             <time>2024-12-31 11:45</time>
           </p>
-          <p className="summary">AI is all the rage — particularly text-generating AI, also known as large language models (think models along the lines of ChatGPT). In one recent survey of ~1,000 enterprise organizations, 67.2% say that they see adopting large language models (LLMs) as a top priority by early 2024.</p>
+          <p className="summary">AI is all the rage — particularly text-generating AI, also known as Large Language Models (think models along the lines of ChatGPT). In one recent survey of ~1,000 enterprise organizations, 67.2% say that they see adopting large language models (LLMs) as a top priority by early 2024.</p>
         </div>
       </div>
   )
